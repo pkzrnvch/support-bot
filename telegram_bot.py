00c0bfceabce54f694227c5e3e33b92c315c2794
pkzrnvch <pkazarnovich@gmail.com>
2022-06-06 10:50:58 +0500

    Setup DialogFlow

--- telegram_bot.py
+++ telegram_bot.py
@@ -3,6 +3,8 @@ import os
 
 from dotenv import load_dotenv
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from google.cloud import dialogflow
+
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
@@ -10,14 +12,28 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 
+def get_dialogflow_reply(session_id, text):
+    project_id = os.environ.get('PROJECT_ID')
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(project_id, session_id)
+    text_input = dialogflow.TextInput(text=text, language_code='ru')
+    query_input = dialogflow.QueryInput(text=text_input)
+    response = session_client.detect_intent(
+        request={"session": session, "query_input": query_input}
+    )
+    return response.query_result.fulfillment_text
+
+
 def start(update, context):
     """Send greetings message when the command /start is issued."""
-    update.message.reply_text('Hi!')
+    update.message.reply_text('Добрый день! Какой у вас вопрос?')
 
 
-def echo(update, context):
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
+def tg_dialogflow_reply(update, context):
+    """DialogFLow reply to the user message."""
+    update.message.reply_text(get_dialogflow_reply(
+        update.effective_user.id,
+        update.message.text))
 
 
 def main():
@@ -26,7 +42,8 @@ def main():
     updater = Updater(tg_bot_token)
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    dispatcher.add_handler(
+        MessageHandler(Filters.text & ~Filters.command, tg_dialogflow_reply))
     updater.start_polling()
     updater.idle()
 
